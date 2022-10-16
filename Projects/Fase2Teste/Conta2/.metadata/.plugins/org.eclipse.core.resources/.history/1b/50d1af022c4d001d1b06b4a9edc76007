60e692ad904b63412a6b3028d608deb581d2e6fe
Vitor <vitor.velozo2003@gmail.com>
2022-10-16 08:04:25 -0300

    Add fase2test

---
+++ Projects/Fase2Teste/Conta2/.metadata/.plugins/org.eclipse.core.resources/.history/1b/50d1af022c4d001d1b06b4a9edc76007
@@ -0,0 +1,5 @@
+package br.com.cesarschool.poo.entidades;
+
+public enum EscoreConta {
+	INDISPONIVEL, BRONZE, PRATA, OURO, DIAMANTE
+}
---
+++ Projects/Fase2Teste/Conta2/ModeloConta2/src/br/com/cesarschool/poo/telas/ProgramaCadastroProduto.java
@@ -0,0 +1,9 @@
+package br.com.cesarschool.poo.telas;
+
+public class ProgramaCadastroProduto {
+
+	public static void main(String[] args) {
+		TelaProduto tela = new TelaProduto();
+		tela.executaTela();
+	}
+}
