714ead0c5a645b5f852d9727fd013b2f8be38f27
Gustavo Henrique <ghco@cesar.school>
2022-10-16 02:15:53 -0300

    TelaConta para Correntista e Conta Poupança

--- Projects/Fase3/AtividadeContinuadaFase3/src/br/com/cesarschool/poo/entidades/Correntista.java
+++ Projects/Fase3/AtividadeContinuadaFase3/src/br/com/cesarschool/poo/entidades/Correntista.java
@@ -1,12 +1,21 @@
 package br.com.cesarschool.poo.entidades;
 
-public class Correntista {
+import java.time.LocalDate;
+
+public class Correntista extends Conta{
 	private String cpf;
 	private String nome;
 	
 	
-	Correntista(String cpf, String nome) {
-	    super();
+	public Correntista(
+			String cpf,
+			String nome,
+			long numero,
+			Status status,
+			LocalDate dataAbertura,
+			TipoConta tipoConta
+			) {
+	    super(numero, status, dataAbertura, tipoConta);
 	    this.cpf = cpf;
 	    this.nome = nome;
 	
